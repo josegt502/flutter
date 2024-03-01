ec5da199833df66e4d30066eaa18d8099f465828
josegt502 <68062175+josegt502@users.noreply.github.com>
2024-03-01 15:55:44 -0600

    FLUTTER-DART03_map2

--- Dart/01_hello_world.dart
+++ Dart/01_hello_world.dart
@@ -1,6 +1,6 @@
 void main() {
-  //late final String nombre;
-  //nombre = 'Fernando';
+  //late final String nombre; late permite dar valor a la variable luego de declararla
+  //nombre = 'Fernando'; ya esta declarada la variable nombre como un string
 const nombre = 'Fernando';
     print('Hola $nombre');
 
